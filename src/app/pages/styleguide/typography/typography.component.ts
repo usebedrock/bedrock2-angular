11ff08f51cedddfb9b716839f146d1e08e3d0dcb
Emily <evdw@mono.company>
2020-06-23 17:48:40 +0200

    Experiment with data

--- src/app/pages/styleguide/typography/typography.component.ts
+++ src/app/pages/styleguide/typography/typography.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { typographyStyles } from '../../../data/typography';
 @Component({
   selector: 'app-typography',
   templateUrl: './typography.component.html',
   styleUrls: ['./typography.component.scss']
 })
 export class TypographyComponent implements OnInit {
-
+  styles = typographyStyles;
   constructor() { }
 
   ngOnInit(): void {
