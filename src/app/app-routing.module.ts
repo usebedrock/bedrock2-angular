61b3e028a374042eca86fc4d3667f74d35fee161
Emily <evdw@mono.company>
2020-06-23 12:32:30 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
-import { PrototypeWrapperComponent } from './prototype-wrapper/prototype-wrapper.component';
+import { ButtonComponent } from './components/button/button.component';
+import { PrototypeWrapperComponent } from './components/prototype-wrapper/prototype-wrapper.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
-    PrototypeWrapperComponent
+    PrototypeWrapperComponent,
+    SignInComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
