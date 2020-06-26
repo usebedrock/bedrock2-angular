78a02f1926cb18367193a17ecab0b6eecd40e6a4
Emily <evdw@mono.company>
2020-06-26 12:49:29 +0200

    Improve styling

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
+  styles: [
+    '.c-app-title { text-decoration: none; font-size: 1.8rem; font-weight: 500;}'
+  ]
 })
 export class DashboardComponent implements OnInit {
 
