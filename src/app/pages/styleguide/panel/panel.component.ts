f7758d207dd615639ed2fb73cf3dde7a5bf13895
Emily <evdw@mono.company>
2020-06-26 11:19:13 +0200

    fix wrong place of code

--- src/app/pages/styleguide/panel/panel.component.ts
+++ src/app/pages/styleguide/panel/panel.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './panel.component.html',
-  host: {'class': 'c-panel__header'}
 })
 export class PanelComponent implements OnInit {
 
