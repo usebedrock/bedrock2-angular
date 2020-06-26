2185a2b528af2f27e2a23522bdc1b804b9517172
Tom Vanhouche <tom.vanhouche@insit.eu>
2020-06-26 15:14:07 +0200

    10 use prism to highlight code

--- src/app/components/styleguide-sample/styleguide-sample.component.ts
+++ src/app/components/styleguide-sample/styleguide-sample.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core';
+import Prism from 'prismjs';
 
 @Component({
   selector: 'br-styleguide-sample',
   templateUrl: './styleguide-sample.component.html',
 })
-export class StyleguideSampleComponent implements OnInit {
+export class StyleguideSampleComponent {
   @Input() title?: string = 'default';
   @Input() code?: string;
   isVisible: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public get highlightedCode(): string {
+    return Prism.highlight(this.code, Prism.languages.html, 'html');
   }
 
+  constructor() { }
 }
